393fe0e7734e664f0f6a49eae33156a692a9119e
Chaz Straney <chaz@jackrussellsoftware.com>
2012-10-05 18:35:34 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pomodomo::Application.config.secret_token = '4c3a747fb543c6d167bbe36375944513ee734a51eca0b7fd67a6625e00b4ea2c4f5f08950987d3b5c153f561656755a29afed2551d9f1e7396ca90be433da535'
